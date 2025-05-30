9489db4487db5a4f3df019e371b2ef5b86415896
Skeptical <161164507+skepticalmc@users.noreply.github.com>
2025-05-30 09:21:37 +0100

    Add Folia Support

--- src/main/java/me/itsskeptical/displaytags/nametags/NametagScheduler.java
+++ src/main/java/me/itsskeptical/displaytags/nametags/NametagScheduler.java
@@ -1,33 +1,55 @@
 package me.itsskeptical.displaytags.nametags;
 
+import io.papermc.paper.threadedregions.scheduler.GlobalRegionScheduler;
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import me.itsskeptical.displaytags.DisplayTags;
+import me.itsskeptical.displaytags.utils.PluginHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitTask;
 
 public class NametagScheduler {
     private final DisplayTags plugin;
     private final NametagManager nametags;
-    private BukkitTask task;
+    private final boolean isFolia;
+
+    private BukkitTask bukkitTask;
+    private ScheduledTask foliaTask;
 
     public NametagScheduler(DisplayTags plugin) {
         this.plugin = plugin;
         this.nametags = DisplayTags.getNametagManager();
+        this.isFolia = PluginHelper.isFolia();
     }
 
     public void start() {
         plugin.getLogger().info("Starting Nametag scheduler...");
         int interval = plugin.config().getNametagUpdateInterval();
-        task = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
-            for (Nametag nametag : nametags.getAll()) {
-                nametag.updateForAll();
-            }
-        }, interval, interval);
+
+        if (isFolia) {
+            GlobalRegionScheduler scheduler = Bukkit.getGlobalRegionScheduler();
+            foliaTask = scheduler.runAtFixedRate(
+                    plugin,
+                    (scheduledTask) -> {
+                        for (Nametag nametag : nametags.getAll()) {
+                            nametag.updateForAll();
+                        }
+                    }, interval, interval
+            );
+        } else {
+            bukkitTask = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+                for (Nametag nametag : nametags.getAll()) {
+                    nametag.updateForAll();
+                }
+            }, interval, interval);
+        }
     }
 
     public void stop() {
-        if (task != null) {
-            plugin.getLogger().info("Stopping Nametag scheduler...");
-            task.cancel();
+        plugin.getLogger().info("Stopping Nametag scheduler...");
+        if (isFolia && foliaTask != null) {
+            foliaTask.cancel();
+        } else if (bukkitTask != null) {
+            bukkitTask.cancel();
         }
     }
 }
---
+++ src/main/java/me/itsskeptical/displaytags/utils/PluginHelper.java
@@ -0,0 +1,12 @@
+package me.itsskeptical.displaytags.utils;
+
+public class PluginHelper {
+    public static boolean isFolia() {
+        try {
+            Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+}
