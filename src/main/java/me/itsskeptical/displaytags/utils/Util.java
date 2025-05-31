53b15b69a657612d48b01fab2954b29c59e02c3b
Skeptical <161164507+skepticalmc@users.noreply.github.com>
2025-05-31 18:02:58 +0100

    Delete PluginHelper and move into Util

--- src/main/java/me/itsskeptical/displaytags/utils/Util.java
+++ src/main/java/me/itsskeptical/displaytags/utils/Util.java
@@ -1,6 +1,12 @@
 package me.itsskeptical.displaytags.utils;
 
-public class PluginHelper {
+import me.itsskeptical.displaytags.utils.helpers.PlaceholdersHelper;
+
+public class Util {
+    public static void load() {
+        PlaceholdersHelper.load();
+    }
+
     public static boolean isFolia() {
         try {
             Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
