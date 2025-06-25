c12dafc8fd588a77767037612f2c8493447e069a
Skeptical <161164507+skepticalmc@users.noreply.github.com>
2025-06-25 20:04:30 +0100

    Add TAB as a (optional) dependency

--- src/main/java/me/itsskeptical/displaytags/utils/handlers/NametagHandler.java
+++ src/main/java/me/itsskeptical/displaytags/utils/handlers/NametagHandler.java
@@ -3,63 +3,58 @@ package me.itsskeptical.displaytags.utils.handlers;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.wrapper.PacketWrapper;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerTeams;
+import me.itsskeptical.displaytags.DisplayTags;
 import me.itsskeptical.displaytags.utils.helpers.DependencyHelper;
 import me.neznamy.tab.api.TabAPI;
 import me.neznamy.tab.api.TabPlayer;
+import me.neznamy.tab.api.event.player.PlayerLoadEvent;
 import me.neznamy.tab.api.nametag.NameTagManager;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 
 public class NametagHandler {
-    public static void hide(Player target, Player viewer) {
+    public static void load() {
         if (DependencyHelper.isTABEnabled()) {
-            TabAPI api = TabAPI.getInstance();
-            NameTagManager manager = api.getNameTagManager();
-            TabPlayer tabTarget = api.getPlayer(target.getUniqueId());
-            TabPlayer tabViewer = api.getPlayer(viewer.getUniqueId());
-            if (manager != null && !manager.hasHiddenNameTag(tabTarget, tabViewer)) {
-                manager.hideNameTag(tabTarget, tabViewer);
-            }
-        } else {
-            String name = getTeamName(target);
-            WrapperPlayServerTeams.ScoreBoardTeamInfo teamInfo = new WrapperPlayServerTeams.ScoreBoardTeamInfo(
-                    Component.empty(),
-                    Component.empty(),
-                    Component.empty(),
-                    WrapperPlayServerTeams.NameTagVisibility.NEVER,
-                    WrapperPlayServerTeams.CollisionRule.ALWAYS,
-                    null,
-                    WrapperPlayServerTeams.OptionData.NONE
-            );
-            WrapperPlayServerTeams packet = new WrapperPlayServerTeams(
-                    name,
-                    WrapperPlayServerTeams.TeamMode.CREATE,
-                    teamInfo,
-                    target.getName()
-            );
-            sendPacket(packet, viewer);
+            TabAPI.getInstance().getEventBus().register(PlayerLoadEvent.class, (event) -> {
+                TabPlayer player = event.getPlayer();
+                NameTagManager manager = TabAPI.getInstance().getNameTagManager();
+
+                if (DisplayTags.getInstance().config().getNametagConfig().isEnabled() && manager != null) {
+                    manager.hideNameTag(player);
+                }
+            });
         }
     }
 
+    public static void hide(Player target, Player viewer) {
+        String name = getTeamName(target);
+        WrapperPlayServerTeams.ScoreBoardTeamInfo teamInfo = new WrapperPlayServerTeams.ScoreBoardTeamInfo(
+                Component.empty(),
+                Component.empty(),
+                Component.empty(),
+                WrapperPlayServerTeams.NameTagVisibility.NEVER,
+                WrapperPlayServerTeams.CollisionRule.ALWAYS,
+                null,
+                WrapperPlayServerTeams.OptionData.NONE
+        );
+        WrapperPlayServerTeams packet = new WrapperPlayServerTeams(
+                name,
+                WrapperPlayServerTeams.TeamMode.CREATE,
+                teamInfo,
+                target.getName()
+        );
+        sendPacket(packet, viewer);
+    }
+
     public static void show(Player target, Player viewer) {
-        if (DependencyHelper.isTABEnabled()) {
-            TabAPI api = TabAPI.getInstance();
-            NameTagManager manager = api.getNameTagManager();
-            TabPlayer tabTarget = api.getPlayer(target.getUniqueId());
-            TabPlayer tabViewer = api.getPlayer(viewer.getUniqueId());
-            if (manager != null && manager.hasHiddenNameTag(tabTarget, tabViewer)) {
-                manager.showNameTag(tabTarget, tabViewer);
-            }
-        } else {
-            String name = getTeamName(target);
-            WrapperPlayServerTeams packet = new WrapperPlayServerTeams(
-                    name,
-                    WrapperPlayServerTeams.TeamMode.REMOVE,
-                    (WrapperPlayServerTeams.ScoreBoardTeamInfo) null,
-                    target.getName()
-            );
-            sendPacket(packet, viewer);
-        }
+        String name = getTeamName(target);
+        WrapperPlayServerTeams packet = new WrapperPlayServerTeams(
+                name,
+                WrapperPlayServerTeams.TeamMode.REMOVE,
+                (WrapperPlayServerTeams.ScoreBoardTeamInfo) null,
+                target.getName()
+        );
+        sendPacket(packet, viewer);
     }
 
     private static String getTeamName(Player target) {
